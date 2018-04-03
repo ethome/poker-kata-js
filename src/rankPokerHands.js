963dbd220347bb9c42a06c43f36a6214a84e7096
Ed Thome <ethome@thoughtworks.com>
2018-04-03 19:51:24 -0400

    Higher card wins

--- src/rankPokerHands.js
+++ src/rankPokerHands.js
@@ -1,5 +1,47 @@
-const rankPokerHands = (first, second) => {
-  return 'Gamblers never win'
+const getValue = function (card) {
+  if (card.substring(0, 2) === '10') {
+    return 10
+  } else if (card[0] === 'J') {
+    return 11
+  } else if (card[0] === 'Q') {
+    return 12
+  } else if (card[0] === 'K') {
+    return 13
+  } else if (card[0] === 'A') {
+    return 14
+  }
+  
+  return parseInt(card[0])
+}
+
+const sortDecrementing = (firstCard, secondCard) => getValue(secondCard) - getValue(firstCard)
+
+const getStringRepresentation = (card) => {
+  if (card[0] === 'J') {
+    return 'Jack'
+  } else if (card[0] === 'Q') {
+    return 'Queen'
+  } else if (card[0] === 'K') {
+    return 'King'
+  } else if (card[0] === 'A') {
+    return 'Ace'
+  }
+  
+  return getValue(card)
+}
+
+const rankPokerHands = (firstHand, secondHand) => {
+  const firstCards = firstHand.split(' ')
+  const firstSorted = firstCards.sort(sortDecrementing)
+  const firstHighCard = firstSorted[0]
+  
+  const secondCards = secondHand.split(' ')
+  const secondSorted = secondCards.sort(sortDecrementing)
+  const secondHighCard = secondSorted[0]
+
+  return getValue(firstHighCard) > getValue(secondHighCard)
+    ? `First hand wins. - with high card: ${getStringRepresentation(firstHighCard)}`
+    : `Second hand wins. - with high card: ${getStringRepresentation(secondHighCard)}`
 }
 
 export default rankPokerHands
--- src/rankPokerHands.test.js
+++ src/rankPokerHands.test.js
@@ -1,12 +1,66 @@
 import rankPokerHands from './rankPokerHands'
 
 describe('rankPokerHands', () => {
-  test('higher numerical card wins', () => {
+  test('higher single-digit numerical card wins', () => {
     let sixHigh = '2H 3D 4S 5C 6D'
     let sevenHigh = '2C 3H 4H 5D 7D'
 
     const result = rankPokerHands(sixHigh, sevenHigh)
 
     expect(result).toEqual('Second hand wins. - with high card: 7')
+  })
+  
+  test('high card can be in any position in hand', () => {
+    let nineHigh = '2H 9D 4S 5C 6D'
+    let eightHigh = '2C 3H 4H 8S 7D'
+  
+    const result = rankPokerHands(nineHigh, eightHigh)
+  
+    expect(result).toEqual('First hand wins. - with high card: 9')
+  })
+  
+  test('ten beats single-digit high card', () => {
+    let nineHigh = '2H 9D 4S 5C 6D'
+    let tenHigh = '10C 3H 4H 8S 7D'
+  
+    const result = rankPokerHands(nineHigh, tenHigh)
+  
+    expect(result).toEqual('Second hand wins. - with high card: 10')
+  })
+  
+  test('jack beats ten', () => {
+    let jackHigh = '2H 9D JS 5C 6D'
+    let tenHigh = '10C 3H 4H 8S 7D'
+  
+    const result = rankPokerHands(jackHigh, tenHigh)
+  
+    expect(result).toEqual('First hand wins. - with high card: Jack')
+  })
+  
+  test('queen beats jack', () => {
+    let jackHigh = '2H 9D JS 5C 6D'
+    let queenHigh = '10C QH 4H 8S 7D'
+  
+    const result = rankPokerHands(jackHigh, queenHigh)
+  
+    expect(result).toEqual('Second hand wins. - with high card: Queen')
+  })
+
+  test('king beats queen', () => {
+    let kingHigh = 'KH 9D JS 5C 6D'
+    let queenHigh = '10C QH 4H 8S 7D'
+  
+    const result = rankPokerHands(kingHigh, queenHigh)
+  
+    expect(result).toEqual('First hand wins. - with high card: King')
+  })
+
+  test('ace beats king', () => {
+    let kingHigh = 'KH 9D JS 5C 6D'
+    let queenHigh = '10C QH 4H AS 7D'
+  
+    const result = rankPokerHands(kingHigh, queenHigh)
+  
+    expect(result).toEqual('Second hand wins. - with high card: Ace')
   })
 })
